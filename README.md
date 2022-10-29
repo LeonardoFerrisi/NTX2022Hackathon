9612f4476577dd0f6aa61db361186a0f0c6be176
LeonardoFerrisi <biggestresurgence1492@gmail.com>
2022-10-29 12:45:27 -0400

    Added connection

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # NTX2022Hackathon
+
+### In Progress Project in Automating usage of ML with EEG data
+
+# Tasks:
+
+- Setup connection scripts that function in streaming EEG
+- Setup ML script that uses brainflow's builtin classifier
+- Setup ML script that adds new EEG data and uses it as training data
+- Setup ML script that uses new script to generate an ML Model
+- Setup ML script that uses ML Model as a classifer for brainflow
---
+++ src/testing/brainflow_ml_test.py
@@ -0,0 +1,48 @@
+import argparse
+import time
+
+from brainflow.board_shim import BoardShim, BrainFlowInputParams, LogLevels
+from brainflow.data_filter import DataFilter
+from brainflow.ml_model import MLModel, BrainFlowMetrics, BrainFlowClassifiers, BrainFlowModelParams
+
+
+def main():
+    BoardShim.enable_board_logger()
+    DataFilter.enable_data_logger()
+    MLModel.enable_ml_logger()
+    
+    params = BrainFlowInputParams()
+
+    board = BoardShim(-1, params)
+    master_board_id = board.get_board_id()
+    sampling_rate = BoardShim.get_sampling_rate(master_board_id)
+    board.prepare_session()
+    board.start_stream(45000, args.streamer_params)
+    BoardShim.log_message(LogLevels.LEVEL_INFO.value, 'start sleeping in the main thread')
+    time.sleep(5)  # recommended window size for eeg metric calculation is at least 4 seconds, bigger is better
+    data = board.get_board_data()
+    board.stop_stream()
+    board.release_session()
+
+    eeg_channels = BoardShim.get_eeg_channels(int(master_board_id))
+    bands = DataFilter.get_avg_band_powers(data, eeg_channels, sampling_rate, True)
+    feature_vector = bands[0]
+    print(feature_vector)
+
+    mindfulness_params = BrainFlowModelParams(BrainFlowMetrics.MINDFULNESS.value,
+                                              BrainFlowClassifiers.DEFAULT_CLASSIFIER.value)
+    mindfulness = MLModel(mindfulness_params)
+    mindfulness.prepare()
+    print('Mindfulness: %s' % str(mindfulness.predict(feature_vector)))
+    mindfulness.release()
+
+    restfulness_params = BrainFlowModelParams(BrainFlowMetrics.RESTFULNESS.value,
+                                              BrainFlowClassifiers.DEFAULT_CLASSIFIER.value)
+    restfulness = MLModel(restfulness_params)
+    restfulness.prepare()
+    print('Restfulness: %s' % str(restfulness.predict(feature_vector)))
+    restfulness.release()
+
+
+if __name__ == "__main__":
+    main()
