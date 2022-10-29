5b6ae4a3d3dd7d0b58ca05fcaba9534cda68f607
LeonardoFerrisi <biggestresurgence1492@gmail.com>
2022-10-29 12:24:08 -0400

    Added connection

---
+++ src/testing/connection.py
@@ -0,0 +1,25 @@
+import time
+from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds, BrainFlowPresets
+
+# ------------------------------------------------------
+SYNTH = -1
+CYTON = 0
+
+def connect_and_read_data():
+    params = BrainFlowInputParams()
+    # params.serial_port = ""
+
+    board = BoardShim(SYNTH, params)
+    board.prepare_session()
+    board.start_stream ()
+    time.sleep(10)
+    # data = board.get_current_board_data (256) # get latest 256 packages or less, doesnt remove them from internal buffer
+    data = board.get_board_data()  # get all data and remove it from internal buffer
+    board.stop_stream()
+    board.release_session()
+
+    print(data)
+
+
+if __name__ == "__main__":
+    connect_and_read_data()
